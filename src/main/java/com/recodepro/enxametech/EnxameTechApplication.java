806e0a0e875b0720a86ce9485446aab8dacfc216
Aira Arima <arimacoelhoaira@gmail.com>
2023-11-06 20:17:53 -0300

    primeiro commit

---
+++ src/main/java/com/recodepro/enxametech/EnxameTechApplication.java
@@ -0,0 +1,13 @@
+package com.recodepro.enxametech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnxameTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnxameTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recodepro/enxametech/EnxameTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recodepro.enxametech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnxameTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
