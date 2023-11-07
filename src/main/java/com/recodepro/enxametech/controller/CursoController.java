5917919c420c51222efb1ff9d9822c63fb7e0a04
alynh0 <99299491+alynh0@users.noreply.github.com>
2023-11-07 16:46:38 -0300

    Curso controller

--- src/main/java/com/recodepro/enxametech/controller/CursoController.java
+++ src/main/java/com/recodepro/enxametech/controller/CursoController.java
@@ -1,2 +1,69 @@
-package com.recodepro.enxametech.controller;public class CursoController {
+package com.recodepro.enxametech.controller;
+
+import com.recodepro.enxametech.repository.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.recodepro.enxametech.model.Curso;
+
+import java.util.Optional;
+
+
+@Controller
+public class CursoController {
+
+    private final CursoRepository cursoRepository;
+
+    @Autowired
+    public CursoController (CursoRepository cursoRepository) {
+        this.cursoRepository =  cursoRepository;
+    }
+
+    @GetMapping("/cursos")
+    public ModelAndView todosCursos() {
+        ModelAndView mv = new ModelAndView(cursos/lista_cursos);
+
+        mv.addObject("curso", cursoRepository.findAll());
+        return mv;
+    }
+
+    @GetMapping("/cursos/{id}")
+    public ModelAndView detalhesCurso(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView(cursos/detalhes);
+
+        Optional<Curso> cursoOptional = cursoRepository.findById(id);
+
+        if (cursoOptional.isPresent()) {
+            Curso curso = cursoOptional.get();
+            mv.addObject("curso", cursoOptional.get());
+        } else {
+            return new ModelAndView("cursos/not_found");
+        }
+
+        return mv;
+    }
+
+    @GetMapping("/cursos/cadastrarCurso")
+    public ModelAndView cadastrarCurso() {
+        ModelAndView mv = new ModelAndView("cursos/formulario");
+        mv.addObject("curso", new Curso());
+        return mv;
+    }
+
+    @GetMapping("/cursos/{id}/editar")
+    public ModelAndView editar(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("cursos/formulario");
+        mv.addObject("curso", cursoRepository.getReferenceById(id));
+        return mv;
+    }
+
+    @PostMapping({ "/cursos/cadastrar", "/cursos/{id}/editar"})
+    public String salvar(Curso curso) {
+        cursoRepository.save(curso);
+        return "redirect:/cursos";
+    }
+
 }
--- src/main/java/com/recodepro/enxametech/repository/CursoRepository.java
+++ src/main/java/com/recodepro/enxametech/repository/CursoRepository.java
@@ -1,2 +1,8 @@
-package com.recodepro.enxametech.repository;public class CursoRepository {
+package com.recodepro.enxametech.repository;
+
+import com.recodepro.enxametech.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
 }
