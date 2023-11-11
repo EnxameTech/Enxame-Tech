800e47be03eca37c5496ec829daafb5576315277
Aira Arima <arimacoelhoaira@gmail.com>
2023-11-11 10:53:09 -0300

    estruturação de pastas: front e back

--- src/main/java/com/recodepro/enxametech/repository/CursoFavoritoRepository.java
+++ src/main/java/com/recodepro/enxametech/repository/CursoFavoritoRepository.java
@@ -1,13 +1,13 @@
-package com.recodepro.enxametech.repository;
-
-import com.recodepro.enxametech.model.Aluno;
-import com.recodepro.enxametech.model.Curso;
-import com.recodepro.enxametech.model.CursoFavorito;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoFavoritoRepository extends JpaRepository<CursoFavorito, Integer> {
-    // vai os cursos favoritos de um determinado aluno
-    CursoFavorito findById_aluno(Aluno id_aluno);
-    // vai procurar os alunos que favoritaram este curso
-    CursoFavorito findById_curso(Curso id_curso);
-}
+//package com.recodepro.enxametech.repository;
+//
+//import com.recodepro.enxametech.model.Aluno;
+//import com.recodepro.enxametech.model.Curso;
+//import com.recodepro.enxametech.model.CursoFavorito;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface CursoFavoritoRepository extends JpaRepository<CursoFavorito, Integer> {
+//    // vai os cursos favoritos de um determinado aluno
+//    CursoFavorito findById_aluno(Aluno id_aluno);
+//    // vai procurar os alunos que favoritaram este curso
+//    CursoFavorito findById_curso(Curso id_curso);
+//}
