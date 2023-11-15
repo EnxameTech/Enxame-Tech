5c8629fc5b5c90a2de23c3d6e11884070e268bdc
Alysson <99299491+alynh0@users.noreply.github.com>
2023-11-15 18:39:55 -0300

    Criação do controller do dashboard

---
+++ src/main/java/com/recodepro/enxametech/controller/DashboardController.java
@@ -0,0 +1,17 @@
+package com.recodepro.enxametech.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+    @GetMapping
+    public ModelAndView dashboard() {
+        ModelAndView mv = new ModelAndView("dashboard/dashbord-home");
+        return mv;
+    }
+}
