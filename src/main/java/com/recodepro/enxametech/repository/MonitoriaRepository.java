6a1ba67ec0d41755f15790108d48d9cdd33eab88
Alysson <99299491+alynh0@users.noreply.github.com>
2023-11-16 19:12:38 -0300

    Update MonitoriaRepository.java

--- src/main/java/com/recodepro/enxametech/repository/MonitoriaRepository.java
+++ src/main/java/com/recodepro/enxametech/repository/MonitoriaRepository.java
@@ -1,7 +1,11 @@
-//package com.recodepro.enxametech.repository;
-//
-//import com.recodepro.enxametech.model.Monitoria;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface MonitoriaRepository extends JpaRepository<Monitoria, Long> {
-//}
+package com.recodepro.enxametech.repository;
+
+import com.recodepro.enxametech.model.Aluno;
+import com.recodepro.enxametech.model.Monitoria;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MonitoriaRepository extends JpaRepository<Monitoria, Long> {
+    List<Monitoria> findByAlunoId(Aluno alunoId);
+}
