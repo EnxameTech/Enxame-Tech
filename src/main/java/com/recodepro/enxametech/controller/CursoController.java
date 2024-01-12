b510efdc3d6f642bfa2dba916a268e830444960e
Aira Arima <arimacoelhoaira@gmail.com>
2024-01-12 18:17:18 -0300

    correção de ortografia no cursoController

--- src/main/java/com/recodepro/enxametech/controller/CursoController.java
+++ src/main/java/com/recodepro/enxametech/controller/CursoController.java
@@ -14,11 +14,7 @@ import java.util.List;
 public class CursoController {
 
     @Autowired
-    private final CursoService cs;
-
-    public CursoController(CursoService cs) {
-        this.cs = cs;
-    }
+    private CursoService cs;
 
     @GetMapping("/listar")
     public List<Curso> listar(){
